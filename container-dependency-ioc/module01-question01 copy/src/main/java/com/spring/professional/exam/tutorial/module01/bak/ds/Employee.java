290fb840f2da23e69f914221e757961162ee6667
Diallo Bakary <bakary.diallo.simplon@gmail.com>
2022-12-16 17:08:25 +0100

    clean folder

--- container-dependency-ioc/module01-question01 copy/src/main/java/com/spring/professional/exam/tutorial/module01/bak/ds/Employee.java
+++
@@ -1,4 +0,0 @@
-package com.spring.professional.exam.tutorial.module01.bak.ds;
-
-public class Employee {
-}
--- container-dependency-ioc/module01-question01 copy/src/main/java/com/spring/professional/exam/tutorial/module01/bak/spring/dependency/injection/Configuration.java
+++
@@ -1,7 +0,0 @@
-package com.spring.professional.exam.tutorial.module01.bak.spring.dependency.injection;
-
-import org.springframework.context.annotation.ComponentScan;
-
-@ComponentScan("com.spring.professional.exam.tutorial.module01.bak")
-public class Configuration {
-}
